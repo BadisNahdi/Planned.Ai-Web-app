0e78c38fc47d0329d4e72c01fe2dbfafd2b20ffa
BadisNahdi <badis.nahdi@insat.ucar.tn>
2024-05-25 17:40:09 +0100

    add env

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TripPlanComponent } from './components/trip-plan/trip-plan.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 const routes: Routes = [
-  { path: '', component: MainPageComponent },
+  { path: '', component: HomepageComponent },
+  { path: 'home', component: HomepageComponent },
+  { path: 'create-new-trip', component: MainPageComponent },
   { path: 'a', component: TripPlanComponent }
 ];
 
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private url: any = 'http://192.168.1.14:3000'
   constructor(
     private http: HttpClient
   ) { }
   createatrip(options: any): Observable<any> {
-    return this.http.post(this.url+'/plan', options)
+    return this.http.post(environment.apiUrl + '/plan', options)
   }
 }
